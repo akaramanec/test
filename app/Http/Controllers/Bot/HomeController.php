713a9c8cc817962adce77fb84e167bbd4d76dff8
Олександр <akaramanec@gmail.com>
2025-01-31 17:43:55 +0200

    add restaurant_id v2

--- app/Http/Controllers/Bot/HomeController.php
+++ app/Http/Controllers/Bot/HomeController.php
@@ -5,21 +5,12 @@ namespace App\Http\Controllers\Bot;
 use App\Bot\TmInfo;
 use App\Http\Controllers\Controller;
 use App\Services\BotTestService;
+use Illuminate\Encryption\Encrypter;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function test()
     {
-        $data = [
-            'dishes' => [
-                ['id' => 1, 'name' => 'Pizza'],
-                ['id' => 2, 'name' => 'Burger'],
-                ['id' => 3, 'name' => 'Pasta'],
-                ['id' => 4, 'name' => 'French Fries'],
-                ['id' => 5, 'name' => 'Sandwich'],
-            ]
-        ];
-        return response()->json($data);
     }
 }
