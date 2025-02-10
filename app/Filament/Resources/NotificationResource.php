cc5de49856299980bb398b9fbab2b62ea90c535a
Олександр <akaramanec@gmail.com>
2025-02-10 18:51:21 +0200

    add Notification pages

---
+++ app/Filament/Resources/NotificationResource.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\NotificationResource\Pages;
+use App\Filament\Resources\NotificationResource\RelationManagers;
+use App\Models\Project\Notification;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class NotificationResource extends Resource
+{
+    protected static ?string $model = Notification::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id')
+                    ->label('ID')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('key')
+                    ->label('Key')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('data')
+                    ->label('Дані')
+                    ->view('components.formatted-data-column')
+                    ->sortable(false),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListNotifications::route('/'),
+            'create' => Pages\CreateNotification::route('/create'),
+            'edit' => Pages\EditNotification::route('/{record}/edit'),
+        ];
+    }
+}
