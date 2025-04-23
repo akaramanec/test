f48c1fd75f3af658b3e1b17c12b10d2f6c022ba9
mirkotabster <orestnodejs@gmail.com>
2025-04-24 00:44:39 +0300

    Update tabster.php

--- config/tabster.php
+++ config/tabster.php
@@ -1,5 +1,5 @@
 <?php
 return [
-    'url' => env('TABSTER_URL', 'https://major-sloth-present.ngrok-free.app'),
+    'url' => env('TABSTER_URL', 'https://dev.app.server.tabster.com.ua'),
     'token' => env('TOKEN_API'),
 ];
