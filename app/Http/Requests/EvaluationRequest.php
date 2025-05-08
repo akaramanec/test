bd913d41bd2702d14785ed98244ed0b1fb528cd7
Ivan Fedorusov <ifedorusov@gmail.com>
2025-05-08 19:25:05 +0300

    refactor EvaluationRequest.php InEstablishmentRequest.php

--- app/Http/Requests/EvaluationRequest.php
+++ app/Http/Requests/EvaluationRequest.php
@@ -6,12 +6,15 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class EvaluationRequest extends FormRequest
 {
+    protected string $required = 'required';
+    protected string $string = 'string';
+
     public function rules(): array
     {
         return [
-            'visitor_id' => ['required', 'string'],
-            'order_id' => ['required', 'string'],
-            'evaluate' => ['required', 'string'],
+            'visitor_id' => [$this->required, $this->string],
+            'order_id' => [$this->required, $this->string],
+            'evaluate' => [$this->required, $this->string],
         ];
     }
 }
--- app/Http/Requests/InEstablishmentRequest.php
+++ app/Http/Requests/InEstablishmentRequest.php
@@ -7,44 +7,62 @@ use Illuminate\Validation\Rule;
 
 class InEstablishmentRequest extends FormRequest
 {
+    protected string $required = 'required';
+    protected string $nullable = 'nullable';
+    protected string $array = 'array';
+    protected string $string = 'string';
+    protected string $numeric = 'numeric';
+    protected string $integer = 'integer';
+    protected string $dateFormat = 'date_format:Y-m-d H:i:s';
+    
+    protected string $requiredWith = 'required_with';
+    protected string $requiredWithDishList;
+    protected string $min0 = 'min:0';
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->requiredWithDishList = $this->requiredWith . ':dish_list';
+    }
+
     public function rules(): array
     {
         return [
-            'reservation_id' => ['required', 'string'],
+            'reservation_id' => [$this->required, $this->string],
 
             // client data
-            'user' => ['required', 'array'],
-            'user.id' => ['required', 'string'],
-            'user.name' => ['required', 'string'],
-            'come_time' => ['required', 'date_format:Y-m-d H:i:s'],
+            'user' => [$this->required, $this->array],
+            'user.id' => [$this->required, $this->string],
+            'user.name' => [$this->required, $this->string],
+            'come_time' => [$this->required, $this->dateFormat],
 
             // order data
-            'order_id' => ['required', 'string'],
+            'order_id' => [$this->required, $this->string],
 
             // table data
-            'table' => ['required', 'array'],
-            'table.id' => ['required', 'string'],
-            'table.name' => ['required', 'string'],
+            'table' => [$this->required, $this->array],
+            'table.id' => [$this->required, $this->string],
+            'table.name' => [$this->required, $this->string],
 
             // dishes data
-            'dish_list' => ['required', 'array'],
-            'dish_list.name' => ['required', 'string'],
-            'dish_list.dishes' => ['array'],
-            'dish_list.dishes.*.id' => ['required', 'string'],
-            'dish_list.dishes.*.name' => ['required', 'string'],
-            'dish_list.dishes.*.price' => ['required', 'numeric'],
-            'dish_list.dishes.*.count' => ['required', 'integer'],
+            'dish_list' => [$this->nullable, $this->array],
+            'dish_list.name' => [$this->requiredWithDishList, $this->string],
+            'dish_list.dishes' => [$this->requiredWithDishList, $this->array, $this->min0],
+            'dish_list.dishes.*.id' => [$this->requiredWithDishList, $this->string],
+            'dish_list.dishes.*.name' => [$this->requiredWithDishList, $this->string],
+            'dish_list.dishes.*.price' => [$this->requiredWithDishList, $this->numeric],
+            'dish_list.dishes.*.count' => [$this->requiredWithDishList, $this->integer],
 
             // workers data
-            'workers' => ['required', 'array'],
-            'workers.admins' => ['required', 'array'],
-            'workers.admins.*.id' => ['required', 'string'],
-            'workers.admins.*.name' => ['required', 'string'],
-            'workers.admins.*.phone' => ['required', 'string'],
-            'workers.waiters' => ['required', 'array'],
-            'workers.waiters.*.id' => ['required', 'string'],
-            'workers.waiters.*.name' => ['required', 'string'],
-            'workers.waiters.*.phone' => ['required', 'string'],
+            'workers' => [$this->required, $this->array],
+            'workers.admins' => [$this->required, $this->array],
+            'workers.admins.*.id' => [$this->required, $this->string],
+            'workers.admins.*.name' => [$this->required, $this->string],
+            'workers.admins.*.phone' => [$this->required, $this->string],
+            'workers.waiters' => [$this->required, $this->array],
+            'workers.waiters.*.id' => [$this->required, $this->string],
+            'workers.waiters.*.name' => [$this->required, $this->string],
+            'workers.waiters.*.phone' => [$this->required, $this->string],
         ];
     }
 }
