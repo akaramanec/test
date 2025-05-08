c186bc6ceed21915a26257b35efeba09278ff629
Ivan Fedorusov <ifedorusov@gmail.com>
2025-05-08 16:45:59 +0300

    refactor OrderRequest.php

--- app/Http/Requests/OrderRequest.php
+++ app/Http/Requests/OrderRequest.php
@@ -8,36 +8,57 @@ class OrderRequest extends FormRequest
 {
     public function rules(): array
     {
+        $routeName = request()->route()->getName();
+        $isPayOrPaid = $routeName == 'order.pay' || $routeName == 'order.paid';
+        $isOrderCall = $routeName == 'order.call';
+        $isOrderAdd = $routeName == 'order.add';
+
+        $required = 'required';
+        $nullable = 'nullable';
+        $array = 'array';
+        $string = 'string';
+        $numeric = 'numeric';
+        $integer = 'integer';
+
+        $callRequired = $isOrderCall ? $nullable : $required;
+        $addRequired = $isOrderAdd ? $required : $nullable;
+        $payRequired = $isPayOrPaid ? $required : $nullable;
+
         return [
             // client data
-            'user' => ['required', 'array'],
-            'user.id' => ['required', 'string'],
-            'user.name' => ['required', 'string'],
+            'user' => [$required, $array],
+            'user.id' => [$required, $string],
+            'user.name' => [$required, $string],
 
             // order data
-            'order_id' => ['nullable', 'string'],
+            'order_id' => [$nullable, $string],
 
             // table data
-            'table' => ['required', 'array'],
-            'table.id' => ['required', 'string'],
-            'table.name' => ['required', 'string'],
+            'table' => [$required, $array],
+            'table.id' => [$required, $string],
+            'table.name' => [$required, $string],
 
             // dishes data
-            'dish_list' => [request()->route()->getName() == 'order.call' ? 'nullable' : 'required', 'array'],
-            'dish_list.name' => [request()->route()->getName() == 'order.call' ? 'nullable' : 'required', 'string'],
-            'dish_list.dishes' => [request()->route()->getName() == 'order.call' ? 'nullable' : 'required', 'array'],
-            'dish_list.dishes.*.id' => [request()->route()->getName() == 'order.call' ? 'nullable' : 'required', 'string'],
-            'dish_list.dishes.*.name' => [request()->route()->getName() == 'order.call' ? 'nullable' : 'required', 'string'],
-            'dish_list.dishes.*.price' => [request()->route()->getName() == 'order.call' ? 'nullable' : 'required', 'numeric'],
-            'dish_list.dishes.*.count' => [request()->route()->getName() == 'order.call' ? 'nullable' : 'required', 'integer'],
-            'dish_list.add_dishes' => [request()->route()->getName() == 'order.add' ? 'required' : 'nullable', 'array'],
-            'dish_list.add_dishes.*.id' => [request()->route()->getName() == 'order.add' ? 'required' : 'nullable', 'string'],
-            'dish_list.add_dishes.*.name' => [request()->route()->getName() == 'order.add' ? 'required' : 'nullable', 'string'],
-            'dish_list.add_dishes.*.price' => [request()->route()->getName() == 'order.add' ? 'required' : 'nullable', 'numeric'],
-            'dish_list.add_dishes.*.count' => [request()->route()->getName() == 'order.add' ? 'required' : 'nullable', 'integer'],
+            'dish_list' => [$callRequired, $array],
+            'dish_list.name' => [$callRequired, $string],
+
+            'dish_list.dishes' => [$callRequired, $array],
+            'dish_list.dishes.*.id' => [$callRequired, $string],
+            'dish_list.dishes.*.name' => [$callRequired, $string],
+            'dish_list.dishes.*.price' => [$callRequired, $numeric],
+            'dish_list.dishes.*.count' => [$callRequired, $integer],
+            
+            'dish_list.add_dishes' => [$addRequired, $array],
+            'dish_list.add_dishes.*.id' => [$addRequired, $string],
+            'dish_list.add_dishes.*.name' => [$addRequired, $string],
+            'dish_list.add_dishes.*.price' => [$addRequired, $numeric],
+            'dish_list.add_dishes.*.count' => [$addRequired, $integer],
 
             // workers data
-            'waiter_id' => ['required', 'string'],
+            'waiter_id' => [$required, $string],
+
+            // payment data
+            'pay_type' => [$payRequired, $string],
         ];
     }
 }
